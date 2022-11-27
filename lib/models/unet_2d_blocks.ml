f67d7e7b0756799c7967da6f7d5a79c219294848
Arulselvan Madhavan <arulselvan1234@gmail.com>
2022-11-27 12:44:47 -0800

    Unet

--- lib/models/unet_2d_blocks.ml
+++ lib/models/unet_2d_blocks.ml
@@ -44,3 +44,48 @@ module Downsample2D = struct
         else Layer.forward conv xs)
   ;;
 end
+
+module Upsample2D = struct
+  type t = { conv : Nn.t }
+
+  let make vs in_channels out_channels =
+    let conv =
+      Layer.conv2d
+        Var_store.(vs / "conv")
+        ~ksize:(3, 3)
+        ~stride:(1, 1)
+        ~padding:(1, 1)
+        ~input_dim:in_channels
+        out_channels
+    in
+    { conv }
+  ;;
+
+  let forward t xs size =
+    let xs =
+      Option.fold
+        size
+        ~none:
+          (let _bsize, _channels, h, w = Tensor.shape4_exn xs in
+           Tensor.upsample_nearest2d
+             xs
+             ~output_size:[ 2 * h; 2 * w ]
+             ~scales_h:(Some 2.)
+             ~scales_w:(Some 2.))
+        ~some:(fun (h, w) ->
+          Tensor.upsample_nearest2d xs ~output_size:[ h; w ] ~scales_h:None ~scales_w:None)
+    in
+    Layer.forward t.conv xs
+  ;;
+end
+
+module DownEncoderBlock2DConfig = struct
+  type t =
+    { num_layers : int
+    ; resnet_eps : float
+    ; resnet_groups : int
+    ; output_scale_factor : float
+    ; add_downsample : bool
+    ; downsample_padding : int
+    }
+end
