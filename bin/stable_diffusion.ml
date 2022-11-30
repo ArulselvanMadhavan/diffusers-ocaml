b649eb03f3a9d1be8efc2d5445412ba110e8cc26
Arulselvan Madhavan <arulselvan1234@gmail.com>
2022-11-30 10:40:33 -0800

    Stable diff

--- bin/stable_diffusion.ml
+++ bin/stable_diffusion.ml
@@ -10,16 +10,18 @@ let run_stable_diffusion prompt cpu =
   let clip_device = cpu_or_cuda "clip" in
   let vae_device = cpu_or_cuda "vae" in
   let unet_device = cpu_or_cuda "unet" in
-  Printf.printf "%s\n" prompt;
   List.iter
     (fun d -> Printf.printf "%b\n" (Device.is_cuda d))
     [ clip_device; vae_device; unet_device ];
   let _tokenizer =
     Diffusers_transformers.Clip.Tokenizer.make "data/bpe_simple_vocab_16e6.txt"
   in
-  (* let scheduler = DDIMScheduler.make n_steps 1000 *)
+  let open Lwt.Syntax in
+  let+ _ = Lwt_log.info (Printf.sprintf "Running with prompt:%s\n" prompt) in
   ()
 ;;
+
+let run_stable_diff prompt cpu = Lwt_main.run (run_stable_diffusion prompt cpu)
 
 let () =
   let open Cmdliner in
@@ -41,8 +43,7 @@ let () =
   let doc = "Stable_diffusion: Generate image from text" in
   let man = [ `S "DESCRIPTION"; `P "Turn text into image" ] in
   let cmd =
-    ( Term.(const run_stable_diffusion $ prompt $ cpu)
-    , Cmd.info "generate" ~sdocs:"" ~doc ~man )
+    Term.(const run_stable_diff $ prompt $ cpu), Cmd.info "generate" ~sdocs:"" ~doc ~man
   in
   let default_cmd = Term.(ret (const (`Help (`Pager, None)))) in
   let info =
