cdf95f1bd8f63cf1742aefac9af51d2b223914d6
Arulselvan Madhavan <arulselvan1234@gmail.com>
2022-12-02 11:27:10 -0800

    SD all

--- lib/pipelines/stable_diffusion.ml
+++ lib/pipelines/stable_diffusion.ml
@@ -1,19 +1,10 @@
 open Torch
 
-(* pub fn build_clip_transformer( *)
-(*     clip_weights: &str, *)
-(*     device: Device, *)
-(* ) -> anyhow::Result<clip::ClipTextTransformer> { *)
-(*     let mut vs = nn::VarStore::new(device); *)
-(*     let text_model = clip::ClipTextTransformer::new(vs.root()); *)
-(*     vs.load(clip_weights)?; *)
-(*     Ok(text_model) *)
-(* } *)
-
 let build_clip_transformer ~clip_weights ~device =
   let vs = Var_store.create ~device ~name:"" () in
-  let _text_model = Diffusers_transformers.Clip.ClipTextTransformer.make vs in
+  let text_model = Diffusers_transformers.Clip.ClipTextTransformer.make vs in
   let named_tensors = Var_store.all_vars vs in
   List.iter (fun (n, _t) -> Printf.printf "Named %s\n" n) named_tensors;
-  Serialize.load_multi_ ~named_tensors ~filename:clip_weights
+  Serialize.load_multi_ ~named_tensors ~filename:clip_weights;
+  text_model
 ;;
--- lib/pipelines/stable_diffusion.mli
+++ lib/pipelines/stable_diffusion.mli
@@ -1,3 +1,6 @@
 open Torch
 
-val build_clip_transformer : clip_weights:string -> device:Device.t -> unit
+val build_clip_transformer
+  :  clip_weights:string
+  -> device:Device.t
+  -> Diffusers_transformers.Clip.ClipTextTransformer.t
